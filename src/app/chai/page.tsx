ae197b730f14e6df23c59ce56c13de5edefd8d8a
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-07-22 23:58:56 +0530

    first commit

--- src/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ChaiPage(){
-    return(
-        <h1 className="text-2xl text-center mt-8">Saurav Kumar from Bulandshahr</h1>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Saurav Kumar</h1>
+    <main className="min-h-screen bg-black/[0.9] antialiase bg-grid-white/[0.02] ">
+      <h1 className="text-2xl text-center">Saurav Kumar</h1>
+      <HeroSection />
+    </main>
   );
 }
